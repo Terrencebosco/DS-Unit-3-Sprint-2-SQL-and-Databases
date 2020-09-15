2bb6021e29443982b4c9f616a5e255389cdf5b41
Terrence Bosco <terrencebosco@gmail.com>
2020-09-14 23:04:35 -0400

    updated lecture notes with new commands

--- module2-sql-for-analysis/elephant_queries.py
+++ module2-sql-for-analysis/elephant_queries.py
@@ -11,8 +11,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 
 ### Connect to ElephantSQL-hosted PostgreSQL
-connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
-                        password=DB_PASSWORD, host=DB_HOST)
+connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
 print('CONNECTION', connection)
 
 ### A "cursor", a structure to iterate over db records to perform queries
@@ -27,3 +26,35 @@ result = cursor.fetchall()
 print(result)
 
 #### in sqlite we can chain exicute and fetch ####
+
+# insert data to the tabel
+
+insert_query = '''
+INSERT INTO test_table (name, data) VALUES
+(
+  'A row name',
+  null
+),
+(
+  'Another row, with JSON',
+  '{ "a": 1, "b": ["dog", "cat", 42], "c": true }'::JSONB
+);
+'''
+
+###################### new to psycopg3 #####################
+
+# run the query
+cursor.execute(insert_query)
+
+# commit the query 
+connection.commit()
+
+# close the cursor
+cursor.close()
+
+# close the connection
+connection.close()
+
+# print the new 
+result = cursor.fetchall()
+print(result)
